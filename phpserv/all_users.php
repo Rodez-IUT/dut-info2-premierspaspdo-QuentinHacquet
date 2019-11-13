784b4d22d4df728c21cc813ba302abafa4c5d2a2
QuentinHacquet <quentin.hacquet@iut-rodez.fr>
2019-11-13 11:30:22 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

---
+++ phpserv/all_users.php
@@ -0,0 +1,45 @@
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Les excuses du lundi matin</title>
+	  
+		<link href="css/monStyle.css" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="fontawesome-free-5.10.2-web/css/all.css" rel="stylesheet">
+	</head>
+	<body>
+		<?php 
+			$host='localhost';
+			$db='my_activities';
+			$user='root';
+			$pass='root';
+			$charset='utf8mb4';
+			$dsn="mysql:host=$host;dbname=$db;charset=$charset";
+			$options=[
+				PDO::ATTR_ERRMODE				=>PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE	=>PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES		=>false,];
+			try{
+				$pdo=new PDO($dsn,$user,$pass,$options);
+			}catch(PDOException$e){
+				throw new PDOException($e->getMessage(),(int)$e->getCode());
+			}
+			$stmt = $pdo->query('SELECT users.id, users.username, users.email, status.name FROM users JOIN status on users.status_id=status.id ORDER BY status.name');
+			echo "<div class=\"container\">";
+			echo "<div class=\"row\">";
+				echo "<div class=\"col-md-3\"><strong>ID</strong></div>";
+				echo "<div class=\"col-md-3\"><strong>Username</strong></div>";
+				echo "<div class=\"col-md-3\"><strong>Email</strong></div>";
+				echo "<div class=\"col-md-3\"><strong>Status</strong></div>";
+			while($row = $stmt->fetch()){
+				echo "<div class=\"col-md-3\">".$row['id']."</div>";
+				echo "<div class=\"col-md-3\">".$row['username']."</div>";
+				echo "<div class=\"col-md-3\">".$row['email']."</div>";
+				echo "<div class=\"col-md-3\">".$row['name']."</div>";
+			}
+			echo "</div>";
+		?>
+	</body>
+</html>
