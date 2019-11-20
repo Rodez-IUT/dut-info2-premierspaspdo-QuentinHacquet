1a32b8de5b2446bc73e93fec0005e1a01236c7ef
QuentinHacquet <quentin.hacquet@iut-rodez.fr>
2019-11-20 11:07:21 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- phpserv/all_users.php
+++ phpserv/all_users.php
@@ -1,57 +1,74 @@
-<html lang="fr">
+<!DOCTYPE html>
+<html>
 	<head>
-		<meta charset="utf-8">
-		<title>Les excuses du lundi matin</title>
-	  
-		<link href="css/monStyle.css" rel="stylesheet">
-		
-		<!-- Bootstrap CSS -->
-		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-		<link href="fontawesome-free-5.10.2-web/css/all.css" rel="stylesheet">
+		<title>All users</title>
+		<meta charset="utf-8 /">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
 	<body>
-		<?php 
-			$var="e%";
-			$statut="Active account";
-
-			$host='localhost';
-			$db='my_activities';
-			$user='root';
-			$pass='root';
-			$charset='utf8mb4';
-			$dsn="mysql:host=$host;dbname=$db;charset=$charset";
-			$options=[
-				PDO::ATTR_ERRMODE				=>PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE	=>PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES		=>false,];
-			try{
-				$pdo=new PDO($dsn,$user,$pass,$options);
-			}catch(PDOException$e){
-				throw new PDOException($e->getMessage(),(int)$e->getCode());
+		<?php
+			$host = 'localhost';
+			$db   = 'my_activities';
+			$user = 'root';
+			$pass = 'root';
+			$charset = 'utf8mb4';
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+			try {
+				 $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+				 throw new PDOException($e->getMessage(), (int)$e->getCode());
 			}
-			$stmt = $pdo->query('SELECT users.id, users.username, users.email, status.name FROM users JOIN status on users.status_id=status.id where status.name="'.$statut.'" AND users.username like "'.$var.'" ');
-			echo "<div class=\"container\">";
-			echo "<div class=\"row\">
-				<div class=\"col-md-6\">
-					<label>Type de médicament :</label> 
-							<select ID="Type" name="Type" class="form-control">
-								echo "<option>"Active account"</option>";
-								echo "<option>"Active account"</option>";
-								echo "<option>"Active account"</option>";
-							</select>
-				</div>";	
-			echo "<div class=\"row\">";
-				echo "<div class=\"col-md-3\"><strong>ID</strong></div>";
-				echo "<div class=\"col-md-3\"><strong>Username</strong></div>";
-				echo "<div class=\"col-md-3\"><strong>Email</strong></div>";
-				echo "<div class=\"col-md-3\"><strong>Status</strong></div>";
-			while($row = $stmt->fetch()){
-				echo "<div class=\"col-md-3\">".$row['id']."</div>";
-				echo "<div class=\"col-md-3\">".$row['username']."</div>";
-				echo "<div class=\"col-md-3\">".$row['email']."</div>";
-				echo "<div class=\"col-md-3\">".$row['name']."</div>";
-			}
-			echo "</div>";
 		?>
+		
+		<h1>All users</h1>
+		<form method="post" action="all_users.php">
+			Start with letter :
+			<input type="text" name="start_letter" />
+			and status is :
+			<select name="account_status">
+				<option value="active">Active account</option>
+				<option value="waiting">Waiting for account validation</option>
+			</select>
+			<input type="submit" value="OK" />
+		</form>
+		<br /><br />
+		<table class="table">
+		<tr>
+			<th>ID</th>
+			<th>Username</th>
+			<th>Email</th>
+			<th>Status</th>
+		</tr>
+		<?php	
+			if (isset($_POST['start_letter']) && isset($_POST['account_status'])) {
+				if (strlen($_POST['start_letter']) == 1) {
+					$start_letter = $_POST['start_letter'];
+				} else {
+					$start_letter = "";
+				}
+				if (strcmp($_POST['account_status'], "active") == 0) {
+					$account_status = 2;
+				} else {
+					$account_status = 1;
+				}
+				$stmt = $pdo->query("SELECT users.id, username, email, name FROM users JOIN status ON status.id = users.status_id WHERE status_id = $account_status AND username LIKE '$start_letter%' ORDER BY username");
+			} else {
+				$stmt = $pdo->query("SELECT users.id, username, email, name FROM users JOIN status ON status.id = users.status_id ORDER BY username");
+			}
+			while ($row = $stmt->fetch()) {
+				echo '<tr>';
+				echo '<td>' . $row['id'] . '</td>';
+				echo '<td>' . $row['username'] . '</td>';
+				echo '<td>' . $row['email'] . '</td>';
+				echo '<td>' . $row['name'] . '</td>';
+				echo '</tr>';
+			}
+		?>
+		</table>
 	</body>
 </html>
