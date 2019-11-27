0abfc3d5dc77a7c0d9c7597d6f5846339e7c64fa
QuentinHacquet <quentin.hacquet@iut-rodez.fr>
2019-11-27 12:06:51 +0100

    activite5.3

--- my_activities_users.sql
+++ my_activities_users.sql
@@ -1,11 +1,149 @@
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('bobdeniro', 1, 'bobdeniro@hollywood.com', 1);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('eliseB', 2, 'eliseb@ihm.com', 1);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('arthurH', 3, 'arthurH@michelin.com', 1);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('wandaL', 4, 'wandal@lalaland.com', 2);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('paulsimon', 5, 'paulsimon@guitare.org', 2);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('jessicaA', 6, 'jessicaa@hollywood.com', 2);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('steveM', 7, 'stevem@cars.com', 2);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('evaM', 8, 'evam@movieland.com', 2);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('alpacino', 9, 'alpacino@moviecountry.com', 2);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('eddym', 10, 'eddym@beverly.com', 3);
-INSERT INTO my_activities.users (username, id, email, status_id) VALUES ('francoises', 11, 'francoises@quaidesbrumes.org', 3);
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mer 27 Novembre 2019 à 11:44
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `my_activities`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `action_log`
+--
+
+CREATE TABLE `action_log` (
+  `id` bigint(20) NOT NULL,
+  `action_date` datetime NOT NULL,
+  `action_name` varchar(50) NOT NULL,
+  `user_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'Waiting for account validation'),
+(2, 'Active account'),
+(3, 'Waiting for account deletion');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `id` bigint(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status_id` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`username`, `id`, `email`, `status_id`) VALUES
+('bobdeniro', 1, 'bobdeniro@hollywood.com', 1),
+('eliseB', 2, 'eliseb@ihm.com', 1),
+('arthurH', 3, 'arthurH@michelin.com', 1),
+('wandaL', 4, 'wandal@lalaland.com', 2),
+('paulsimon', 5, 'paulsimon@guitare.org', 2),
+('jessicaA', 6, 'jessicaa@hollywood.com', 2),
+('steveM', 7, 'stevem@cars.com', 2),
+('evaM', 8, 'evam@movieland.com', 2),
+('alpacino', 9, 'alpacino@moviecountry.com', 2),
+('eddym', 10, 'eddym@beverly.com', 3),
+('francoises', 11, 'francoises@quaidesbrumes.org', 3);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `action_log`
+--
+ALTER TABLE `action_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `action_log_users_id_fk` (`user_id`);
+
+--
+-- Index pour la table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `status_id_uindex` (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id_uindex` (`id`),
+  ADD KEY `user_status_id_index` (`status_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `action_log`
+--
+ALTER TABLE `action_log`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `action_log`
+--
+ALTER TABLE `action_log`
+  ADD CONSTRAINT `action_log_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `user_status__fk` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
